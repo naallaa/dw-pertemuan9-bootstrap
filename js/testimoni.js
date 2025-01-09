b8fc788a8fda7a865e8d1d0afe47feb39ac4d121
naallaa <nala.yubara@gmail.com>
2025-01-09 09:32:36 +0800

    update sebelum uas

--- js/testimoni.js
+++ js/testimoni.js
@@ -1,9 +1,31 @@
+
 var NIM = "2401010593"
 var urlAPI = "https://apimhstiki.ptov.my.id/"
 var urlTestiRead = urlAPI+"testi-"+NIM+"/read"
+var urlTestiDel = urlAPI+"testi"
 
+$("#infSukses").hide()
+$("#infError").hide()
+
+function hapus(id){
+    $.ajax({
+        url: urlTestiDel,
+        method: 'POST',
+        data: 'ACT=destroy&NIM='+NIM+'&IDX='+id,
+        dataType: 'json',
+        success:function(dt){
+            $("#infSukses").show()
+            $("#infSukses").html("Penghapusan Testimoni sukses")
+            setTimeout(window.location.replace("latihan1301.html"),3000)
+        },
+        error:function(){
+            $("#infError").show()
+            $("#infError").html("Pengapusan Testimoni Gagal")
+            setTimeout(window.location.replace("latihan1301.html"),3000)
+        }
+    })
+}
 $(function(){
-
     $.ajax({
         url: urlTestiRead,
         method: "GET",
@@ -11,18 +33,23 @@ $(function(){
         success:function(dta){
             let tbl = ""
             let ipx = ""
-            if(dta && dta.error==4){
-                 dta.TESTI.forEach(function(isi){
+            let cmdhapus = ""
+            if(dta && ((dta.error==4) || (dta.error==0))){
+                dta.TESTI.forEach(function(isi){
+                    if(dta.error==0){
+                        ipx = isi.IPX
+                        cmdhapus = `<a onclick="hapus('${isi.IDX}')" class="btn btn-danger btn-sm"> Hapus </a>`
+                    } 
                     tbl += `<tr>
-                    <td><img src="${isi.GRAVATAR}" width="75">${isi.NAMA}</td>
-                    <td>${isi.EMAIL}</td>
-                    <td>${isi.TESTI}</td>
-                    <td>${ipx}</td>
-                  </tr>`
+                <td><img src="${isi.GRAVATAR}" width="75"> ${isi.NAMA}</td>
+                <td>${isi.EMAIL}</td>
+                <td>${isi.TESTI}</td>
+                <td>${ipx}</td>
+                <td>${cmdhapus}</td>
+              </tr>`
                 })
             }
-            $("tbody").html(tbl)
-
+            $("tbody").html(tbl) 
         },
         error:function(){
             console.log("Gagal Membaca data testimoni")
